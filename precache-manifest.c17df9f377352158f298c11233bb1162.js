5e3476ef9f1de2c54b2e2dd8f9edda1b8530b5df
Abhishek Doshi <abhishekdoshi94@gmail.com>
2020-01-10 18:38:41 +0530

    Updates

---
+++ precache-manifest.c17df9f377352158f298c11233bb1162.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26ee7e635ec9b3702325f66d534f998",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "5d52d2cee492c0edb624",
+    "url": "/react-todo/static/css/main.1e44daad.chunk.css"
+  },
+  {
+    "revision": "1f99d0e9e6381e14aa37",
+    "url": "/react-todo/static/js/2.9f0ea3e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo/static/js/2.9f0ea3e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d52d2cee492c0edb624",
+    "url": "/react-todo/static/js/main.fd21175c.chunk.js"
+  },
+  {
+    "revision": "84d0b06f9327463f07ca",
+    "url": "/react-todo/static/js/runtime-main.0d9a23df.js"
+  }
+]);
